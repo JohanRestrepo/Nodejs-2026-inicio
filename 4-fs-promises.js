bc57d258cedfd3b047b98cfd2b587ec32281b635
Zarigueya <esnic1994@gmail.com>
2026-04-05 16:33:09 -0500

    probando procesos asincronos

---
+++ 4-fs-promises.js
@@ -0,0 +1,14 @@
+const fs = require('node:fs/promises');
+
+console.log('Leyendo el primer archivo...')
+const text = fs.readFile('./archivo.txt', 'utf-8').then(text => {
+    console.log('Primer Texto:', text);
+});
+
+console.log('haciendo cosas nazis')
+
+console.log('Leyendo el segundo archivo...')
+const text2 = fs.readFile('./archivo2.txt', 'utf-8').then(text2 => {
+    console.log('Segundo Texto:', text2);
+});
+
